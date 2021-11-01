ddb8c39214e80fcd96b5c57134241e177e3057c9
Abdulkadir GÜNGÖR <abdlkdrgngr@gmail.com>
2021-11-01 12:53:53 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Nuxt Strapi</h1>
+    <article v-for="blog in blogs" :key="blog._id">
+      <h2>{{ blog.title }}</h2>
+      <p>{{ blog.body }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      blogs:[],
+      error: ''
+    }
+  },
+  async mounted() {
+    try{
+      this.blogs = await this.$strapi.$blogs.find({_locale:'tr'})
+    }catch(error){
+      this.error = error
+    }
+  }
+}
+</script>
